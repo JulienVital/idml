4f715e68a772aaf134fa65f138e0be539b2dc402
julien <contact@julien-vital.dev>
2022-11-21 21:38:59 +0100

    add tests

--- tests/Properties/PropertiesTest.php
+++ tests/Properties/PropertiesTest.php
@@ -2,6 +2,9 @@
 
 use JMS\Serializer\SerializerBuilder;
 use Jvital\Idml\Properties\Properties;
+use Jvital\Idml\Properties\Value\AppliedFont;
+use Jvital\Idml\Properties\Value\BasedOn;
+use Jvital\Idml\Properties\Value\Leading;
 use PHPUnit\Framework\TestCase;
 
 class PropertiesTest extends TestCase{
@@ -25,5 +28,45 @@ class PropertiesTest extends TestCase{
         $this->assertEquals($propertiesSerialized, $this->xmlExpect['Properties']);
     }
 
-    
+    public function testAppliedFont(){
+        $property = new Properties();
+        $appliedFontValue = new AppliedFont(); 
+        $appliedFontValue->setType('string');
+        $appliedFontValue->setValue('Miriam Libre');
+        $property->setAppliedFont($appliedFontValue);
+
+        $propertySerialized = $this->serializer->serialize($property, 'xml');
+        $propertyDeSerialized = $this->serializer->deSerialize($propertySerialized, Properties::class,'xml');
+
+        $this->assertEquals($propertyDeSerialized->getAppliedFont()->getType(), 'string');
+        $this->assertEquals($propertyDeSerialized->getAppliedFont()->getValue(), 'Miriam Libre');
+    }
+
+    public function testBasedOn(){
+        $property = new Properties();
+        $BasedOnValue = new BasedOn(); 
+        $BasedOnValue->setType('string');
+        $BasedOnValue->setValue('$ID/[No paragraph style]');
+        $property->setBasedOn($BasedOnValue);
+
+        $propertySerialized = $this->serializer->serialize($property, 'xml');
+        $propertyDeSerialized = $this->serializer->deSerialize($propertySerialized, Properties::class,'xml');
+
+        $this->assertEquals($propertyDeSerialized->getBasedOn()->getType(), 'string');
+        $this->assertEquals($propertyDeSerialized->getBasedOn()->getValue(), '$ID/[No paragraph style]');
+    }
+
+    public function testLeading(){
+        $property = new Properties();
+        $leadingValue = new Leading(); 
+        $leadingValue->setType('unit');
+        $leadingValue->setValue(25);
+        $property->setLeading($leadingValue);
+
+        $propertySerialized = $this->serializer->serialize($property, 'xml');
+        $propertyDeSerialized = $this->serializer->deSerialize($propertySerialized, Properties::class,'xml');
+
+        $this->assertEquals($propertyDeSerialized->getLeading()->getType(), 'unit');
+        $this->assertEquals($propertyDeSerialized->getLeading()->getValue(), 25);
+    }
 }
--- tests/Ressources/Styles/ParagraphStyleTest.php
+++ tests/Ressources/Styles/ParagraphStyleTest.php
@@ -52,6 +52,7 @@ class ParagraphStyleTest extends TestCase{
 
         $this->assertEquals($paragraphStyleSerialized, $this->xmlExpect['ParagraphStyle4']);
     }
+
     public function testImported(){
         $paragraphStyle = new ParagraphStyle();
         $paragraphStyle->setImported(false);
@@ -68,5 +69,158 @@ class ParagraphStyleTest extends TestCase{
         $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
 
         $this->assertEquals($paragraphStyleDeSerialized->getNextStyle(), "test name next Style");
+    }
+
+    public function testVerticalScale (){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setVerticalScale (123);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getVerticalScale (), 123);
+    }
+
+    public function testUnderline(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setUnderline(true);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->isUnderline(), true);
+    }
+
+    public function testTracking(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setTracking(123);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getTracking(), 123);
+    }
+
+    public function testStrikeThru(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setStrikeThru(true);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->isStrikeThru(), true);
+    }
+
+    public function testSpaceBefore(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setSpaceBefore(58);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getSpaceBefore(), 58);
+    }
+
+    public function testSpaceAfter(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setSpaceAfter(12);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getSpaceAfter(), 12);
+    }
+
+    public function testSkew(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setSkew(8);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getSkew(), 8);
+    }
+
+    public function testRightIndent(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setRightIndent(8);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getRightIndent(), 8);
+    }
+
+    public function testLeftIndent(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setLeftIndent(8);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getLeftIndent(), 8);
+    }
+
+    public function testPointSize(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setPointSize(11);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getPointSize(), 11);
+    }
+
+    public function testLastLineIndent(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setLastLineIndent(6);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getLastLineIndent(), 6);
+    }
+
+    public function testFirstLineIndent(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setFirstLineIndent(6);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getFirstLineIndent(), 6);
+    }
+
+    public function testJustification(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setJustification("LeftJustified");
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getJustification(), "LeftJustified");
+    }
+
+    public function testCapitalization(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setCapitalization("SmallCaps");
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getCapitalization(), "SmallCaps");
+    }
+    
+    public function testFontStyle(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setFontStyle("Bold");
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getFontStyle(), "Bold");
+    }
+
+    public function testHorizontalScale(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setHorizontalScale(12);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getHorizontalScale(), 12);
+    }
+
+    public function testBaselineShift(){
+        $paragraphStyle = new ParagraphStyle();
+        $paragraphStyle->setBaselineShift(12);
+        $paragraphStyleSerialized = $this->serializer->serialize($paragraphStyle, 'xml');
+        $paragraphStyleDeSerialized = $this->serializer->deSerialize($paragraphStyleSerialized, ParagraphStyle::class,'xml');
+
+        $this->assertEquals($paragraphStyleDeSerialized->getBaselineShift(), 12);
     }
 }
