0f196907ab1418a9df4e05cab98fc38aeede6c94
julien <contact@julien-vital.dev>
2022-11-26 23:45:01 +0100

    base

--- tests/IDML/CreateIdmlTest.php
+++ tests/IDML/CreateIdmlTest.php
@@ -1,22 +1,19 @@
 <?php
 
 use JMS\Serializer\SerializerBuilder;
+use Jvital\Idml\Builder\IdmlDocument;
 use Jvital\Idml\SerializationClass\Designmap\Designmap;
 use PHPUnit\Framework\TestCase;
 
 class CreateIdmlTest extends TestCase{
 
-    /**
-     * @var Serializer
-     */
-    private $serializer;
+
 
     public function __construct(){
         parent::__construct();
-        $this->serializer = SerializerBuilder::create()->build();
     }
 
     function testCreation(){
-        $this->assertTrue(true);
+        $idml = new IdmlDocument();
     }
 }
