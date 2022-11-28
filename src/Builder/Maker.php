44bddf2145bbfdc6dccd2c187313a4c19782cb21
julien <contact@julien-vital.dev>
2022-11-28 23:17:09 +0100

    baseFiles

--- src/Builder/Maker.php
+++ src/Builder/Maker.php
@@ -2,20 +2,36 @@
 namespace Jvital\Idml\Builder;
 
 class Maker{
+    const RAW_FOLDER = '/raw';
     
     private IdmlDocument $document;
 
     public function __construct(IdmlDocument $document){
         $this->document = $document;
-        mkdir($this->getRawFolder(),0777, true);
+        mkdir($this->getRootFolder(),0775, true);
+        mkdir($this->getRawFolder(),0775, true);
     }
 
-    private function getRawFolder():string{
+    private function getRootFolder():string{
         return $this->getDocument()->getTargetFolder().'/'.$this->getDocument()->getName();
     }
 
-    public function exec(){
+    private function getRawFolder():string{
+        return $this->getRootFolder().self::RAW_FOLDER;
+    }
 
+    private function copyMetaInf(){
+        mkdir($this->getRawFolder()."/META-INF",0775, true);
+        copy(__DIR__."/rawFiles/META-INF/container.xml",$this->getRawFolder()."/META-INF/container.xml");
+    }
+
+    private function copyMimetype(){
+        copy(__DIR__."/rawFiles/mimetype",$this->getRawFolder()."/mimetype");
+    }
+
+    public function exec(){
+        $this->copyMetaInf();
+        $this->copyMimetype();
     }
 
 
--- tests/IDML/CreateIdmlTest.php
+++ tests/IDML/CreateIdmlTest.php
@@ -7,29 +7,67 @@ use PHPUnit\Framework\TestCase;
 
 class CreateIdmlTest extends TestCase{
 
-
+  const TARGET_FOLDER = './temp';
 
   public function __construct(){
     parent::__construct();
   }
 
-  function testCreationFolder(){
-    $namefile = 'testFile';
-    $targetFolder = './temp';
+  protected function tearDown():void{
+    $this->deleteRecursiveFolder(self::TARGET_FOLDER);
+  }
+  private function generateBaseDocument(string $nameDocument):IdmlDocument{
 
     $idml = new IdmlDocument();
-    $idml->setName($namefile);
-    $idml->generate($targetFolder);
-    
-    $this->assertTrue(is_dir("./temp/$namefile"));
-    
-    $this->deleteRecursiveFolder($targetFolder);
+    $idml->setName($nameDocument);
+    $idml->generate(self::TARGET_FOLDER);
+    return $idml;
   }
-
+  
   private function deleteRecursiveFolder(string $directory): bool{
 
     array_map(fn (string $file) => is_dir($file) ? $this->deleteRecursiveFolder($file) : unlink($file), glob($directory . '/' . '*'));
 
     return rmdir($directory);
+  }
+
+  public function testCreationFolder(){
+    $namefile = 'testFile';
+
+    $this->generateBaseDocument($namefile);
+    
+    $this->assertTrue(is_dir("./temp/$namefile"));
+  }
+
+  public function testCreationFolderRawFolder(){
+    $namefile = 'testFile';
+
+    $this->generateBaseDocument($namefile);
+    
+    $this->assertTrue(is_dir("./temp/$namefile/raw"));
+  }
+
+  public function testMetaInf(){
+    $namefile = 'testFile';
+
+    $this->generateBaseDocument($namefile);
+    
+    $this->assertTrue(is_dir(self::TARGET_FOLDER."/$namefile/raw/META-INF"));
+  }
+
+  public function testMetaInfContainer(){
+    $namefile = 'testFile';
+
+    $this->generateBaseDocument($namefile);
+    
+    $this->assertTrue(file_exists(self::TARGET_FOLDER."/$namefile/raw/META-INF/container.xml"));
+  }
+
+  public function testMimetype(){
+    $namefile = 'testFile';
+
+    $this->generateBaseDocument($namefile);
+    
+    $this->assertTrue(file_exists(self::TARGET_FOLDER."/$namefile/raw/mimetype"));
   }
 }
