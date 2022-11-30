1ed760e9481a38542546d949062a8e93fa5ba7fe
julien <contact@julien-vital.dev>
2022-11-30 23:57:43 +0100

    wip

--- src/Builder/Maker.php
+++ src/Builder/Maker.php
@@ -1,13 +1,26 @@
 <?php
 namespace Jvital\Idml\Builder;
 
+use ZipArchive;
+
 class Maker{
     const RAW_FOLDER = '/raw';
     
     private IdmlDocument $document;
+    private ZipArchive $zipArchive;
 
     public function __construct(IdmlDocument $document){
         $this->document = $document;
+        $filename = $document->getName() ;
+
+
+        $zip_path = __DIR__."./$filename.zip";
+        $zip = new ZipArchive();
+      
+        if ($zip->open($zip_path, ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE) !== TRUE) {
+          die ("An error occurred creating your ZIP file.");
+        }
+        $zip->close();
         mkdir($this->getRootFolder(),0775, true);
         mkdir($this->getRawFolder(),0775, true);
     }
