70f11c658ccaae67ae7fdbd01cd79b1137fd4a8f
julien <contact@julien-vital.dev>
2023-01-22 23:46:28 +0100

    hyphenation

---
+++ tests/SerializationClass/Trait/HyphenationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+use Jvital\Idml\SerializationClass\Trait\HyphenationAttribute;
+use Jvital\Tests\Helper\XmlTestCase;
+
+Class testHyphenationAttribute{
+    use HyphenationAttribute;
+}
+
+Class HyphenationTest extends XmlTestCase{
+
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    public function __construct(){
+        parent::__construct();
+        $this->serializer = SerializerBuilder::create()->build();
+    }
+
+    public function testSerializeToXml(){
+
+        $myClass = new testHyphenationAttribute();
+
+        $myClass->setHyphenation(true);
+        $xmlSerialized = $this->serialize($myClass, 'xml');
+
+        $expectedXml = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result Hyphenation="true"/>';
+
+        $result = $this->xmlIsSame($expectedXml, $xmlSerialized);
+        $this->assertTrue($result);
+    }
+
+    public function testDeserializeXml(){
+
+        $xmlFile = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result Hyphenation="false"/>';
+
+
+        $objectDeserialized = $this->deSerialize($xmlFile, testHyphenationAttribute::class,'xml');
+
+        $this->assertFalse($objectDeserialized->isHyphenation());
+    }
+
+}
+
