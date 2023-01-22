0eade0df29842712fb11f25ec09006fd00054270
julien <contact@julien-vital.dev>
2023-01-22 20:18:16 +0100

    BottomLeftCornerOption

---
+++ tests/SerializationClass/Trait/BottomLeftCornerOptionAttributeTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+use Jvital\Idml\SerializationClass\Trait\BottomLeftCornerOptionAttribute;
+use Jvital\Tests\Helper\XmlTestCase;
+
+Class testBottomLeftCornerOptionAttribute{
+    use BottomLeftCornerOptionAttribute;
+}
+
+Class BottomLeftCornerOptionAttributeTest extends XmlTestCase{
+
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    public function __construct(){
+        parent::__construct();
+        $this->serializer = SerializerBuilder::create()->build();
+    }
+
+    public function testSerializeToXml(){
+
+        $myClass = new testBottomLeftCornerOptionAttribute();
+
+        $myClass->setBottomLeftCornerOption('test name CornerOption');
+        $xmlSerialized = $this->serialize($myClass, 'xml');
+
+        $expectedXml = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result BottomLeftCornerOption="test name CornerOption"/>';
+
+        $result = $this->xmlIsSame($expectedXml, $xmlSerialized);
+        $this->assertTrue($result);
+    }
+
+    public function testDeserializeXml(){
+
+        $xmlFile = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result BottomLeftCornerOption="test name CornerOption"/>';
+
+
+        $objectDeserialized = $this->deSerialize($xmlFile, testBottomLeftCornerOptionAttribute::class,'xml');
+
+        $this->assertEquals($objectDeserialized->getBottomLeftCornerOption(),"test name CornerOption");
+    }
+
+}
+
