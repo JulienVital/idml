fb5cfeea7fa6b94746b23673d1a68b306fdfaafc
julien <contact@julien-vital.dev>
2023-01-26 23:33:42 +0100

    strike

---
+++ tests/SerializationClass/Trait/StrikeThroughOffsetTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+
+use Jvital\Idml\SerializationClass\Trait\StrikeThroughOffsetAttribute;
+use Jvital\Tests\Helper\XmlTestCase;
+
+Class testStrikeThroughOffsetAttributeTest{
+    use StrikeThroughOffsetAttribute;
+}
+
+Class StrikeThroughOffsetAttributeTest extends XmlTestCase{
+
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    public function __construct(){
+        parent::__construct();
+        $this->serializer = SerializerBuilder::create()->build();
+    }
+
+    public function testSerializeToXml(){
+
+        $myClass = new testStrikeThroughOffsetAttributeTest();
+        $myClass->setStrikeThroughOffset(23.23);
+
+        $xmlSerialized = $this->serialize($myClass, 'xml');
+
+        $expectedXml = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result StrikeThroughOffset="23.23"/>';
+
+        $this->assertXmlStringEqualsXmlString($expectedXml, $xmlSerialized);
+    }
+
+    public function testDeserializeXml(){
+
+        $xmlFile = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result StrikeThroughOffset="39.9"/>';
+
+        $objectDeserialized = $this->deSerialize($xmlFile, testStrikeThroughOffsetAttributeTest::class,'xml');
+    
+        $this->assertEquals($objectDeserialized->getStrikeThroughOffset(), "39.9");
+    }
+
+}
+
---
+++ tests/SerializationClass/Trait/StrikeThroughWeightTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+
+use Jvital\Idml\SerializationClass\Trait\StrikeThroughWeightAttribute;
+use Jvital\Tests\Helper\XmlTestCase;
+
+Class testStrikeThroughWeightAttributeTest{
+    use StrikeThroughWeightAttribute;
+}
+
+Class StrikeThroughWeightAttributeTest extends XmlTestCase{
+
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    public function __construct(){
+        parent::__construct();
+        $this->serializer = SerializerBuilder::create()->build();
+    }
+
+    public function testSerializeToXml(){
+
+        $myClass = new testStrikeThroughWeightAttributeTest();
+        $myClass->setStrikeThroughWeight(48.48);
+
+        $xmlSerialized = $this->serialize($myClass, 'xml');
+
+        $expectedXml = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result StrikeThroughWeight="48.48"/>';
+
+        $this->assertXmlStringEqualsXmlString($expectedXml, $xmlSerialized);
+    }
+
+    public function testDeserializeXml(){
+
+        $xmlFile = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result StrikeThroughWeight="12.899"/>';
+
+        $objectDeserialized = $this->deSerialize($xmlFile, testStrikeThroughWeightAttributeTest::class,'xml');
+    
+        $this->assertEquals($objectDeserialized->getStrikeThroughWeight(), "12.899");
+    }
+
+}
+
