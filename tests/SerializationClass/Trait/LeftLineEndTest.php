3daf4c706eae5f83fcee548f0b740a629c70d261
julien <contact@julien-vital.dev>
2023-01-26 19:48:46 +0100

    indent

---
+++ tests/SerializationClass/Trait/LeftLineEndTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+
+use Jvital\Idml\SerializationClass\Trait\LeftLineEndAttribute;
+use Jvital\Tests\Helper\XmlTestCase;
+
+Class testLeftLineEndTest{
+    use LeftLineEndAttribute;
+}
+
+Class LeftLineEndTest extends XmlTestCase{
+
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    public function __construct(){
+        parent::__construct();
+        $this->serializer = SerializerBuilder::create()->build();
+    }
+
+    public function testSerializeToXml(){
+
+        $myClass = new testLeftLineEndTest();
+        $myClass->setLeftLineEnd('SimpleWideArrowHead');
+
+        $xmlSerialized = $this->serialize($myClass, 'xml');
+
+        $expectedXml = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result LeftLineEnd="SimpleWideArrowHead"/>';
+
+        $this->assertXmlStringEqualsXmlString($expectedXml, $xmlSerialized);
+    }
+
+    public function testDeserializeXml(){
+
+        $xmlFile = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result LeftLineEnd="None"/>';
+
+        $objectDeserialized = $this->deSerialize($xmlFile, testLeftLineEndTest::class,'xml');
+    
+        $this->assertEquals($objectDeserialized->getLeftLineEnd(), "None");
+    }
+
+}
+
---
+++ tests/SerializationClass/Trait/RightLineEndTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+
+use Jvital\Idml\SerializationClass\Trait\RightLineEndAttribute;
+use Jvital\Tests\Helper\XmlTestCase;
+
+Class testRightLineEndTest{
+    use RightLineEndAttribute;
+}
+
+Class RightLineEndTest extends XmlTestCase{
+
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    public function __construct(){
+        parent::__construct();
+        $this->serializer = SerializerBuilder::create()->build();
+    }
+
+    public function testSerializeToXml(){
+
+        $myClass = new testRightLineEndTest();
+        $myClass->setRightLineEnd('SimpleArrowHead');
+
+        $xmlSerialized = $this->serialize($myClass, 'xml');
+
+        $expectedXml = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result RightLineEnd="SimpleArrowHead"/>';
+
+        $this->assertXmlStringEqualsXmlString($expectedXml, $xmlSerialized);
+    }
+
+    public function testDeserializeXml(){
+
+        $xmlFile = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result RightLineEnd="None"/>';
+
+        $objectDeserialized = $this->deSerialize($xmlFile, testRightLineEndTest::class,'xml');
+    
+        $this->assertEquals($objectDeserialized->getRightLineEnd(), "None");
+    }
+
+}
+
