58274ed6ed9f95d471c0fd844ecffdbc97a6af02
julien <contact@julien-vital.dev>
2023-01-28 22:04:15 +0100

    topleftcorne

---
+++ tests/SerializationClass/Trait/topLeftCornerOptionTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+use Jvital\Idml\SerializationClass\Trait\TopLeftCornerOptionAttribute;
+
+use Jvital\Tests\Helper\XmlTestCase;
+
+Class testtopLeftCornerOptionAttributeTest{
+    use TopLeftCornerOptionAttribute;
+}
+
+Class TopLeftCornerOptionTest extends XmlTestCase{
+
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    public function __construct(){
+        parent::__construct();
+        $this->serializer = SerializerBuilder::create()->build();
+    }
+
+    public function testSerializeToXml(){
+
+        $myClass = new testtopLeftCornerOptionAttributeTest();
+        $myClass->settopLeftCornerOption("RoundedCorner");
+
+        $xmlSerialized = $this->serialize($myClass, 'xml');
+
+        $expectedXml = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result TopLeftCornerOption="RoundedCorner"/>';
+
+        $this->assertXmlStringEqualsXmlString($expectedXml, $xmlSerialized);
+    }
+
+    public function testDeserializeXml(){
+
+        $xmlFile = 
+        '<?xml version="1.0" encoding="UTF-8"?>
+            <result TopLeftCornerOption="BevelCorner"/>';
+
+        $objectDeserialized = $this->deSerialize($xmlFile, testtopLeftCornerOptionAttributeTest::class,'xml');
+    
+        $this->assertEquals($objectDeserialized->gettopLeftCornerOption(), "BevelCorner");
+    }
+
+}
+
