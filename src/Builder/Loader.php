53af1142d8adf0f781f4c0808d0e47420e7ca384
julien <contact@julien-vital.dev>
2023-02-06 00:04:43 +0100

    golden test generate for refactor

--- src/Builder/Loader.php
+++ src/Builder/Loader.php
@@ -22,40 +22,51 @@ class Loader{
             $name = $zip->getNameIndex($i);
             $contents = $zip->getFromIndex($i);
     
-            if (strpos($name, 'Spread') === 0){
+            if (strpos($name, 'Spread') === 0 && $name!="Spreads/"){
                 
                 $spreadDeSerialized = $serializer->deSerialize($contents, SpreadIdpkg::class,'xml');
                 $document->addSpreads($spreadDeSerialized);
+                continue;
             }
     
-            if (strpos($name, 'Stories') === 0){
+            if (strpos($name, 'Stories') === 0 && $name!= "Stories/"){
             
                 $storyDeserialized = $serializer->deSerialize($contents, StoryIdpkg::class,'xml');
                 $document->addStory($storyDeserialized);
+                continue;
+
             }
     
             if (strpos($name, 'designmap') === 0){
                 
                 $designMapDeserialized = $serializer->deSerialize($contents, Designmap::class,'xml');
                 $document->setDesignMap($designMapDeserialized);
+                continue;
+
             }
             
             if (strpos($name, "Resources/Fonts.xml") === 0){
                 
                 $font = $serializer->deSerialize($contents, FontIdpkg::class,'xml');
                 $document->setFonts($font);
+                continue;
+
             }
         
             if (strpos($name, "Resources/Graphic.xml") === 0){
                 
                 $graphic = $serializer->deSerialize($contents, GraphicIdpkg::class,'xml');
                 $document->setGraphic($graphic);
+                continue;
+
             }
         
             if (strpos($name, "Resources/Styles.xml") === 0){
                 
                 $styles = $serializer->deSerialize($contents, StylesIdpkg::class,'xml');
                 $document->setStyles($styles);
+                continue;
+
             }
         }
         $zip->close();
--- tests/IDML/LoadIdmlTest.php
+++ tests/IDML/LoadIdmlTest.php
@@ -11,12 +11,36 @@ class LoadIdmlTest extends TestCase{
 
 
   public function testLoadIdml(){
-    $idmlFile = __DIR__ ."/expects/Minimalist Magazine Layout.idml";
+    $idmlFileBase = __DIR__ ."/expects/Minimalist Magazine Layout.idml";
 
     $loader = new Loader();
-    $document = $loader->load($idmlFile);    
+    $document = $loader->load($idmlFileBase);
     $document->generate(__DIR__);
-    // unlink( __DIR__ ."/Minimalist Magazine Layout.idml");
+
+    $idmlZipBase = new ZipArchive;
+    $idmlZipBase->open($idmlFileBase);
+
+    $zipGenerated = new ZipArchive;
+    $zipGenerated->open(__DIR__ ."/Minimalist Magazine Layout.idml");
+    $listFiles=[
+      'Resources/Fonts.xml',
+      'Resources/Graphic.xml',
+      'Resources/Styles.xml',
+      'Spreads/Spread_u4ad.xml',
+      'Spreads/Spread_u5f6.xml',
+      'Stories/Story_u1b4.xml',
+      'Stories/Story_u1ca.xml',
+      'XML/BackingStory.xml',
+      'XML/Tags.xml'
+    ];
+
+    foreach ($listFiles as $nameFile) {
+      $expectContent = $idmlZipBase->getFromName($nameFile);
+      $generatedContent = $zipGenerated->getFromName($nameFile);
+      $this->assertXmlStringEqualsXmlString($generatedContent, $expectContent,"$nameFile Test");
+    }
+
+    unlink( __DIR__ ."/Minimalist Magazine Layout.idml");
   }
   
 
